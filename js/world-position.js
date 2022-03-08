69562b4b9d8be4606cf3a9312cc9b7e25e3a7af8
dustinpfister <dustin.pfister@gmail.com>
2022-03-08 14:07:55 -0500

    parseLatLon

--- js/world-position.js
+++ js/world-position.js
@@ -2,12 +2,27 @@ var WorldPos = (function () {
 
     var api = {};
 
+    // parse a given lat or lon value for a method like fromSea
+    var parseLatLon = function(n){
+        if(n < 0){
+            return 0;
+        }
+        if(n >= 1){
+            return 0.9999999999999999; 
+        }
+        return n;
+    };
+
     var DEFAULT_SEALEVEL = 3;
 
     api.fromSea = function(world, lat, lon, alt){
         var v = new THREE.Vector3();
         // use seaLevel value in userData object of world object, or default value for it
         var seaLevel = world.userData.seaLevel === undefined ? DEFAULT_SEALEVEL : world.userData.seaLevel;
+        // parse lat an lon
+        lat = parseLatLon(lat);
+        lon = parseLatLon(lon);
+/*
         // alt above sea level, lat and lon
         alt = alt === undefined ? 0 : alt;
         lat = lat === undefined ? 0 : lat;
@@ -19,7 +34,8 @@ var WorldPos = (function () {
         // lat and lon values in 0-1 form
         lat = THREE.MathUtils.euclideanModulo(lat, 1);
         lon = THREE.MathUtils.euclideanModulo(lon, 1);
-        
+        */
+
         // set and return position of vector
         var phi = Math.PI * lon;
         var theta = Math.PI * 2 * lat;
