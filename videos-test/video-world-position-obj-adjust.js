b472892b110c528a18c6c140c24dae7f41cd442e
dustinpfister <dustin.pfister@gmail.com>
2022-03-08 15:35:22 -0500

    nice background color

--- videos-test/video-world-position-obj-adjust.js
+++ videos-test/video-world-position-obj-adjust.js
@@ -16,6 +16,8 @@ VIDEO.scripts = [
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
 
+    scene.background = new THREE.Color(0x00afaf)
+
     // CAMERA
     camera.position.set(30, 10, -20);
     camera.lookAt(0, 5, 0);
@@ -29,7 +31,7 @@ VIDEO.init = function(sm, scene, camera){
     light.add(new THREE.PointLight(0xdfdfdf, 0.8));
     light.position.set(0, 50, -50);
     scene.add(light);
-    scene.add(new THREE.AmbientLight(0xffffff, 0.2))
+    scene.add(new THREE.AmbientLight(0xffffff, 0.15))
 
     // WORLD MESH
     let world = scene.userData.world = utils.DAE.getMesh( VIDEO.daeResults[0] );
@@ -70,17 +72,11 @@ VIDEO.update = function(sm, scene, camera, per, bias){
     guy1.moveLegs(sm.per, 4);
     guy1.moveArms(sm.per, 4);
 
-    // testing out the adjust object method
-    //var lat = 1 - sm.per,
-    //lon = 0.35,
-    //alt = 10,
-    //heading = 1.57;
-
     var radian = Math.PI * 2 * sm.per * -1, 
     lat = 0.25 + Math.cos(radian) * 0.10, 
     lon = 0.25 + Math.sin(radian) * 0.10, 
     alt = 10, 
-    heading = radian * -1; //Math.PI * 0 - Math.PI * 2 * sm.per;
+    heading = radian * -1;
 
     WorldPos.adjustObject(world, guy1.group, lat, lon, heading, alt, 'fromSea');
 
