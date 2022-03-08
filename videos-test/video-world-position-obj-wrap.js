ed7b32eafd02153dda2cbb4804bbe8210a531fa7
dustinpfister <dustin.pfister@gmail.com>
2022-03-08 14:38:29 -0500

    video-world-position-obj-wrap test video started

---
+++ videos-test/video-world-position-obj-wrap.js
@@ -0,0 +1,72 @@
+/* world-position-obj-wrap.js - working out an object wrap standard
+ *
+ */
+// the dae files to use for this video
+VIDEO.daePaths = [
+  '../dae/world/world.dae'
+];
+
+VIDEO.scripts = [
+  '../js/world-position.js',
+  '../js/canvas.js',
+  '../js/guy.js',
+  '../js/guy-canvas.js'
+];
+
+// init method for the video
+VIDEO.init = function(sm, scene, camera){
+
+    // CAMERA
+    camera.position.set(0, 0, 35);
+
+    // LIGHT
+    let light = scene.userData.light = new THREE.Mesh(
+        new THREE.SphereGeometry(0.25, 20, 20),
+        new THREE.MeshStandardMaterial({
+            emissive: 0xffffff
+        }));
+    light.add(new THREE.PointLight(0xdfdfdf, 0.8));
+    light.position.set(0, 50, 50);
+    scene.add(light);
+
+    // WORLD MESH
+    let world = scene.userData.world = utils.DAE.getMesh( VIDEO.daeResults[0] );
+    world.material = new THREE.MeshPhongMaterial({
+        color: new THREE.Color('#00ff00'),
+        emissive: new THREE.Color('#ffffff'),
+        emissiveIntensity: 0.2
+    });
+    scene.add(world);
+
+    // Guy 1 obj
+    var guy1 = new Guy();
+    scene.userData.obj1 = guy1.group;
+    guy1.group.rotateX(1.57);
+    scene.add(guy1.group);
+/*
+    var obj1 = scene.userData.obj1 = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 5, 1), 
+        new THREE.MeshNormalMaterial());
+    obj1.geometry.rotateX(1.57);
+    scene.add(obj1);
+*/
+
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    let world = scene.userData.world,
+    obj1 = scene.userData.obj1;
+
+    var lat = sm.per,
+    lon = 0.25 + 0.5 * sm.bias,
+    alt = 10;
+
+    var v = WorldPos.fromSea(world, lat, lon, alt);
+    obj1.position.copy(v);
+    obj1.lookAt(world.position);
+
+    camera.position.copy(v.clone().add(new THREE.Vector3(3,0,0)).normalize().multiplyScalar(25));
+    camera.lookAt(obj1.position);
+};
+
