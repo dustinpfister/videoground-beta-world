65acae8bb62c746557758d40dcfc470c001ab278
dustinpfister <dustin.pfister@gmail.com>
2022-03-12 12:15:38 -0500

    current state of house1 dae loading in video11

--- videos-test/video11-house1.js
+++ videos-test/video11-house1.js
@@ -1,13 +1,15 @@
 
 // the dae files to use for this video
 VIDEO.daePaths = [
-  '../dae/house1/cone.dae'
+  '../dae/house1/house1.dae'
 ];
 
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
+
+    scene.add(new THREE.GridHelper(10, 10));
     // CAMERA
-    camera.position.set(0, 0, -4);
+    camera.position.set(-10, 10, -10);
     camera.lookAt(0, 0, 0)
 
     // LIGHT
@@ -15,21 +17,26 @@ VIDEO.init = function(sm, scene, camera){
     light.position.set(-5, 10, -5)
     scene.add(light);
 
-    // loading world
-    let world = scene.userData.world = utils.DAE.getMesh( VIDEO.daeResults[0] );
-    var map = world.material.map;
+    // loading home1
+    let home1 = scene.userData.home1 = VIDEO.daeResults[0].scene.children[2];
+    home1.position.set(-4,0,0)
+    scene.add(home1);
+
+/*
+    //var map = world.material.map;
     world.material = new THREE.MeshStandardMaterial({
         emissive: 0xffffff,
         emissiveMap: map,
         emissiveIntensity: 0.4
     });
-    scene.add(world);
+*/
+
 
 };
 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
-    let world = scene.userData.world;
-    world.rotation.z = Math.PI * 2 * sm.per;
+    //let home1 = scene.userData.home1;
+    //home1.rotation.z = Math.PI * 2 * sm.per;
 };
 
