4f2f6bcb2415fbad5539d18210af35821eaa5bd7
dustinpfister <dustin.pfister@gmail.com>
2022-03-12 10:36:45 -0500

    video10 is using standard material

--- videos-test/video10-world-texture2.js
+++ videos-test/video10-world-texture2.js
@@ -7,15 +7,22 @@ VIDEO.daePaths = [
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
     // CAMERA
-    camera.position.set(0, 0, -3);
+    camera.position.set(0, 0, -4);
     camera.lookAt(0,0,0)
+
+    // LIGHT
+    var light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.position.set(-5, 10, -5)
+    scene.add(light);
 
     // loading world
     let world = scene.userData.world = utils.DAE.getMesh( VIDEO.daeResults[0] );
     var map = world.material.map;
-    world.material = new THREE.MeshBasicMaterial({
-        map: map
-    })
+    world.material = new THREE.MeshStandardMaterial({
+        emissive: 0xffffff,
+        emissiveMap: map,
+        emissiveIntensity: 0.4
+    });
     scene.add(world);
 
 };
