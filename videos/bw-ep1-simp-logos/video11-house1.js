1ad3de98098d0a86159d54175741b8c3620ce450
dustinpfister <dustin.pfister@gmail.com>
2022-03-14 14:19:43 -0400

    started new video file

---
+++ videos/bw-ep1-simp-logos/video11-house1.js
@@ -0,0 +1,46 @@
+
+// the dae files to use for this video
+VIDEO.daePaths = [
+    '../../dae/house1/house1.dae',
+    '../../dae/desk/desk.dae'
+];
+
+// init method for the video
+VIDEO.init = function(sm, scene, camera){
+
+    scene.add(new THREE.GridHelper(10, 10));
+    // CAMERA
+    camera.position.set(1, 2, 1);
+    camera.lookAt(0.2, 0.0, 0.5);
+
+    // LIGHT
+    var light = new THREE.PointLight(0xffffff, 0.5);
+    light.position.set(0, 0, 0)
+    scene.add(light);
+
+    // loading home1
+    let home1 = scene.userData.home1 = VIDEO.daeResults[0].scene.children[2];
+    home1.position.set(-4,0,0)
+    scene.add(home1);
+
+    let desk = scene.userData.desk = VIDEO.daeResults[1].scene.children[2];
+    desk.position.set(0,1,0)
+    scene.add(desk);
+
+/*
+    //var map = world.material.map;
+    world.material = new THREE.MeshStandardMaterial({
+        emissive: 0xffffff,
+        emissiveMap: map,
+        emissiveIntensity: 0.4
+    });
+*/
+
+
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+
+};
+
