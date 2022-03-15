33ecf287da0e7a361213b686c766a24924b053d2
dustinpfister <dustin.pfister@gmail.com>
2022-03-15 15:10:56 -0400

    video12 test video started

---
+++ videos-test/video12-guy-dae.js
@@ -0,0 +1,41 @@
+/*   video12 - testing out my new guy dae file and how it compares to my old threejs guy
+ */
+// the dae files to use for this video
+VIDEO.daePaths = [
+  '../dae/guy/guy.dae'
+];
+
+VIDEO.scripts = [
+  '../js/canvas.js',
+  '../js/guy.js',
+  '../js/guy-canvas.js',
+  '../js/guy-characters.js'
+];
+
+// init method for the video
+VIDEO.init = function(sm, scene, camera){
+
+    // gird helper
+    scene.add(new THREE.GridHelper(10, 10));
+
+    // CAMERA
+    camera.position.set(1, 5, 5);
+    camera.lookAt(0, 0, 0);
+
+    // LIGHT
+    var light = new THREE.PointLight(0xffffff, 0.5);
+    light.position.set(2, 3, 2)
+    scene.add(light);
+
+    // loading home1
+    let guyDAE = scene.userData.guyDAE = VIDEO.daeResults[0].scene.children[2];
+    guyDAE.position.set(0,0,0);
+    scene.add(guyDAE);
+
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+
+};
+
