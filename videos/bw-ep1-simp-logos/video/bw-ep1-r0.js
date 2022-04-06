448d5039f290587cb5973a77f40e0258a6e9e410
dustinpfister <dustin.pfister@gmail.com>
2022-04-06 14:03:00 -0400

    pos of guy1 looks okay so far

--- videos/bw-ep1-simp-logos/video/bw-ep1-r0.js
+++ videos/bw-ep1-simp-logos/video/bw-ep1-r0.js
@@ -19,17 +19,32 @@ VIDEO.init = function(sm, scene, camera){
     home1.position.set(0, 0, 0);
     scene.add(home1);
 
+    // loading mrguy1 as a guy2 model
     let mrg1 = scene.userData.mrg1 = VIDEO.daeResults[1].scene.children[2];
     DAEHelpers.reMapGroup(mrg1);
-    mrg1.position.set(-2, 4.0, 0);
+    mrg1.position.set(-2.75, 2.25, -3.0);
+
     mrg1.rotation.set(Math.PI * 1.5, 0, Math.PI * 1.0);
     scene.add(mrg1);
+
+    // adjusting fixed pose for mrguy1
+    var pelvis = mrg1.getObjectByName('pelvis');
+    // rotate pelvis and caff
+    pelvis.rotation.set(-1.57, 0, 0);
+
+    var caff1 = pelvis.children[0].children[0];
+    caff1.rotation.set(1.57, 0, 0);
+    var caff2 = pelvis.children[1].children[0];
+    caff2.rotation.set(1.57, 0, 0);
+
+    console.log(mrg1);
+    console.log(pelvis); 
 };
 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
     // camera
-    camera.position.set(4.5, 7.25, 2.5 - 6 * bias);
+    camera.position.set(0.5, 2.0, 2.5 - 8 * bias);
     camera.lookAt(-4, 2, -4 + 4 * bias);
 };
 
