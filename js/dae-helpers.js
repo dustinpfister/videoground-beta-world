0ff5b99fe1b5ce9390ff5f74ad0708cd8b128e8e
dustinpfister <dustin.pfister@gmail.com>
2022-04-06 11:04:17 -0400

    opt object for dae helpers re map method

--- js/dae-helpers.js
+++ js/dae-helpers.js
@@ -1,9 +1,14 @@
 var DAEHelpers = (function () {
-    
+    // API
     var api = {};
-
-    // remape group helper
-    api.reMapGroup = function(group){
+    // DAEHelpers.reMapGroup(group, opt)
+    // remap the map property for all mesh object to the emissiveMap property
+    api.reMapGroup = function(group, opt){
+        // options
+        opt = opt || {};
+        // default to using THREE.DoubleSide for the side prop of materials
+        opt.side = opt.side || THREE.DoubleSide;
+        // for all children of the group
         group.children.forEach(function(child){
             // if child has a material
             if(child.material){
@@ -28,7 +33,7 @@ var DAEHelpers = (function () {
             }
         });
     };
-
+    // return public API
     return api;
 }
     ());
