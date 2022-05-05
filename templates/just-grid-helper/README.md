364999549a903c97b0d0154f9dfebb60859d00fe
dustinpfister <dustin.pfister@gmail.com>
2022-05-05 14:03:24 -0400

    started a templates folder

---
+++ templates/just-grid-helper/README.md
@@ -0,0 +1,3 @@
+# just-grid-helper video 
+
+This is a clean grid helper only template in terms of starting display objects in the scene.
---
+++ templates/just-grid-helper/videos/video1.js
@@ -0,0 +1,48 @@
+// video1 for just-grid-helper
+ 
+// scripts
+VIDEO.scripts = [
+   '../../../js/sequences.js'
+];
+// init
+VIDEO.init = function(sm, scene, camera){
+ 
+    // BACKGROUND
+    scene.background = new THREE.Color('#2a2a2a');
+
+    // GRID
+    var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    scene.add( grid );
+
+    // SET UP SEQ OBJECT
+    sm.seq = Sequences.create({
+        sm: sm,
+        part : [
+            {
+                per: 0,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(8, 1, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            },
+            {
+                per: 0.10,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(8, 1 + 8 * partPer, 0);
+                    camera.lookAt(0, 0, 0);
+                }
+            }
+        ]
+    });
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    // sequences
+    Sequences.update(sm.seq, sm);
+};
+
