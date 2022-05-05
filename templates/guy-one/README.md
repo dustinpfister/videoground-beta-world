2ed215ab77ef2d7211558df856c5fc391ea38271
dustinpfister <dustin.pfister@gmail.com>
2022-05-05 15:35:03 -0400

    set guy pos method

---
+++ templates/guy-one/README.md
@@ -0,0 +1,3 @@
+# guy-one video 
+
+Simple starting template that makes use of the guy.js along with guy-canvas and guy-characters.js. This still aims to be a fairly clean starting point for this that just involves a single instane of the guy.js model.
---
+++ templates/guy-one/videos/video1.js
@@ -0,0 +1,85 @@
+// video1 for guy-one template
+ 
+// scripts
+VIDEO.scripts = [
+    '../../../js/sequences.js',
+    '../../../js/world-position.js',
+    '../../../js/canvas.js',
+    '../../../js/guy.js',
+    '../../../js/guy-canvas.js',
+    '../../../js/guy-characters.js'
+];
+// init
+VIDEO.init = function(sm, scene, camera){
+ 
+    // BACKGROUND
+    scene.background = new THREE.Color('#2a2a2a');
+    var ud = scene.userData;
+
+    var grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    scene.add( grid );
+
+    // light
+    var dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(1, 3, 1);
+    scene.add(dl);
+
+    // CALL create method for guy1
+    GuyCharacters.create(scene, 'guy1');
+    var guy1Obj = ud.obj1;
+    // box helper ( set box.visible = true to see )
+    var box = new THREE.BoxHelper( guy1Obj, 0xffff00 );
+    box.visible = false;
+    scene.add(box);
+    // bounding box - ( trying this out for help with setting y position )
+    var bbox = new THREE.Box3().setFromObject(guy1Obj);
+    var bSize = new THREE.Vector3(); 
+
+    // set guy position in trems of x and z, and 
+    // optional alt if I want him going up into the sky for some reason
+    var setGuyPos = function(x, z, alt){
+        alt = alt === undefined ? 0 : alt;
+        bbox.getSize(bSize);
+        guy1Obj.position.y = bSize.y / 2 + alt;
+    };
+
+    // SET UP SEQ OBJECT
+    sm.seq = Sequences.create({
+        sm: sm,
+        part : [
+            {
+                per: 0,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(10, 10, 10);
+                    camera.lookAt(guy1Obj.position);
+                    // guy1
+                    box.setFromObject(guy1Obj);
+                    bbox.setFromObject(guy1Obj);
+                    setGuyPos(0, 0);
+                }
+            },
+            {
+                per: 0.10,
+                init: function(sm){},
+                update: function(sm, scene, camera, partPer, partBias){
+                    // camera
+                    camera.position.set(10, 10, 10);
+                    camera.lookAt(guy1Obj.position);
+                    // guy1
+                    box.setFromObject(guy1Obj);
+                    bbox.setFromObject(guy1Obj);
+                    setGuyPos(0, 0, 3 * partBias);
+                }
+            }
+        ]
+    });
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    // sequences
+    Sequences.update(sm.seq, sm);
+};
+
