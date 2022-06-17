1ceb9a70ccdb0fe80b43774e4823d47b78ce1658
dustinpfister <dustin.pfister@gmail.com>
2022-06-17 10:51:51 -0400

    first test video started

--- videos-test/video14-werid-face-1.js
+++ videos-test/video14-werid-face-1.js
@@ -6,8 +6,7 @@ VIDEO.daePaths = [
 ];
 
 VIDEO.scripts = [
-  '../js/dae-helpers.js',
-  '../js/canvas.js'
+  '../js/dae-helpers.js'
 ];
 
 // init method for the video
@@ -18,16 +17,18 @@ VIDEO.init = function(sm, scene, camera){
     scene.add(new THREE.GridHelper(10, 10));
 
     // LIGHT
-    var light = new THREE.PointLight(0xffffff, 0.5);
-    light.position.set(0, 2, 0)
+    var light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 2, 3);
     scene.add(light);
+    var ambient = new THREE.AmbientLight(0xffffff, 0.15);
+    scene.add(ambient);
 
     // loading werid face one
     let wf = scene.userData.wf = VIDEO.daeResults[0].scene.children[2];
     wf.position.set(0, 0.25, 0);
     // !!! if werid face one has textures then use this to make them emmisve maps with the standard material
     //DAEHelpers.reMapGroup(guyDAE);
-    scene.add(guyDAE);
+    scene.add(wf);
 
     // get by name
 /*
@@ -41,7 +42,7 @@ VIDEO.init = function(sm, scene, camera){
 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
-    //camera.position.set(-4 + 8 * sm.bias, 2, 9);
-    //camera.lookAt(1, 0, 0);
+    camera.position.set(2, 0, 2 - 4 * bias);
+    camera.lookAt(0, 0, 0);
 };
 
