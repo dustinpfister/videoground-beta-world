94d8f5f8a799b2cc19d71d279346508933bf31e1
dustinpfister <dustin.pfister@gmail.com>
2022-06-17 10:43:11 -0400

    weird face 1

---
+++ videos-test/video14-werid-face-1.js
@@ -0,0 +1,47 @@
+/*   video14 - testing out weird-face-1 dae file
+ */
+// the dae files to use for this video
+VIDEO.daePaths = [
+  '../dae/weird-face-1/weird-face-1.dae'
+];
+
+VIDEO.scripts = [
+  '../js/dae-helpers.js',
+  '../js/canvas.js'
+];
+
+// init method for the video
+VIDEO.init = function(sm, scene, camera){
+
+    // GRID HELPER AND BACKGROUND
+    scene.background = new THREE.Color('black');
+    scene.add(new THREE.GridHelper(10, 10));
+
+    // LIGHT
+    var light = new THREE.PointLight(0xffffff, 0.5);
+    light.position.set(0, 2, 0)
+    scene.add(light);
+
+    // loading werid face one
+    let wf = scene.userData.wf = VIDEO.daeResults[0].scene.children[2];
+    wf.position.set(0, 0.25, 0);
+    // !!! if werid face one has textures then use this to make them emmisve maps with the standard material
+    //DAEHelpers.reMapGroup(guyDAE);
+    scene.add(guyDAE);
+
+    // get by name
+/*
+    var head = guyDAE.getObjectByName('head'); 
+    head.rotation.z = Math.PI / 180 * 120;
+    console.log(guyDAE);
+*/
+
+
+};
+
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    //camera.position.set(-4 + 8 * sm.bias, 2, 9);
+    //camera.lookAt(1, 0, 0);
+};
+
