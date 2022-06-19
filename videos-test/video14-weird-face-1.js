f3fbd974347b6358079fe37b1100ff0869237354
dustinpfister <dustin.pfister@gmail.com>
2022-06-19 15:18:32 -0400

    tried updating normals more than one way but that seems to no nothing

--- videos-test/video14-weird-face-1.js
+++ videos-test/video14-weird-face-1.js
@@ -14,20 +14,36 @@ var lerpGeo = function(geo, geoA, geoB, alpha){
     alpha = alpha || 0;
     // pos, and new pos
     let pos = geo.getAttribute('position');
+    let norm = geo.getAttribute('normal');
+    // positions for a and b
     let posA = geoA.getAttribute('position');
     let posB = geoB.getAttribute('position');
+    // normals for a and b
+    let normA = geoA.getAttribute('normal');
+    let normB = geoB.getAttribute('normal');
+
     var i = 0, len = pos.array.length;
     while(i < len){
+        // update position
         var v = new THREE.Vector3(posA.array[i], posA.array[i + 1], posA.array[i + 2]);
         var v2 = new THREE.Vector3(posB.array[i], posB.array[i + 1], posB.array[i + 2]);
         v.lerp(v2, alpha);
         pos.array[i] = v.x;
         pos.array[i + 1] = v.y;
         pos.array[i + 2] = v.z;
+        // update normals
+       // var v = new THREE.Vector3(normA.array[i], normA.array[i + 1], normA.array[i + 2]);
+       // var v2 = new THREE.Vector3(normB.array[i], normB.array[i + 1], normB.array[i + 2]);
+       // v.lerp(v2, alpha);
+       // norm.array[i] = v.x;
+       // norm.array[i + 1] = v.y;
+       // norm.array[i + 2] = v.z;        
+
         i += 3;
     }
     pos.needsUpdate = true;
     //geo.computeVertexNormals();
+    //norm.needsUpdate = true;
 };
 
 
@@ -54,6 +70,9 @@ VIDEO.init = function(sm, scene, camera){
     let mouth = ud.mouth = nose.getObjectByName('mouth');
     // add just main 'nose' group to scene, m0 and m1 are used to update geo of mouth
     scene.add(nose);
+
+console.log( mouth.geometry.getAttribute('position').array.length )
+console.log( mouth.geometry.getAttribute('normal').array.length )
 
     console.log(mouth.geometry)
 
@@ -110,7 +129,7 @@ lerpGeo(mouth.geometry, m0.geometry, m1.geometry, 0.5)
 VIDEO.update = function(sm, scene, camera, per, bias){
     let ud = scene.userData;
 
-    lerpGeo(ud.mouth.geometry, ud.m0.geometry, ud.m1.geometry, 1);
+    lerpGeo(ud.mouth.geometry, ud.m0.geometry, ud.m1.geometry, bias);
 
     camera.position.set(2 - 4 * bias, 0, 2);
     camera.lookAt(0, -0.25, 0);
