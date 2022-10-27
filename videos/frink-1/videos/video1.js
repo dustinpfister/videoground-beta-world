57b59ce4d8151ede86cd6899b35917636991071b
dustinpfister <dustin.pfister@gmail.com>
2022-10-27 12:20:25 -0400

    started frink1 video folder

---
+++ videos/frink-1/videos/video1.js
@@ -0,0 +1,123 @@
+// 'frink1'
+// scripts
+VIDEO.scripts = [
+   '../../../js/sequences-hooks/r2/sequences-hooks.js',
+   '../../../js/canvas/r1/canvas.js'
+];
+// init
+VIDEO.init = function(sm, scene, camera){
+    //-------- ----------
+    // HELPERS - helper functions from sequence hook demos
+    //   ( see https://dustinpfister.github.io/2022/05/12/threejs-examples-sequence-hooks/ )
+    //-------- ----------
+    // just a short hand for THREE.QuadraticBezierCurve3
+    const QBC3 = function(x1, y1, z1, x2, y2, z2, x3, y3, z3){
+        let vs = x1;
+        let ve = y1;
+        let vc = z1;
+        if(arguments.length === 9){
+            vs = new THREE.Vector3(x1, y1, z1);
+            ve = new THREE.Vector3(x2, y2, z2);
+            vc = new THREE.Vector3(x3, y3, z3);
+        }
+        return new THREE.QuadraticBezierCurve3( vs, vc, ve );
+    };
+    // QBDelta helper using QBC3
+    // this works by giving deltas from the point that is half way between
+    // the two start and end points rather than a direct control point for x3, y3, and x3
+    const QBDelta = function(x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+        const vs = new THREE.Vector3(x1, y1, z1);
+        const ve = new THREE.Vector3(x2, y2, z2);
+        // deltas
+        const vDelta = new THREE.Vector3(x3, y3, z3);
+        const vc = vs.clone().lerp(ve, 0.5).add(vDelta);
+        const curve = QBC3(vs, ve, vc);
+        return curve;
+    };
+    // QBV3Array
+    const QBV3Array = function(data) {
+        const v3Array = [];
+        data.forEach( ( a ) => {
+            const curve = QBDelta.apply(null, a.slice(0, a.length - 1))
+            v3Array.push( curve.getPoints( a[9]) );
+        })
+        return v3Array.flat();
+    };
+    //-------- ----------
+    // BACKGROUND
+    //-------- ----------
+    scene.background = new THREE.Color('#2a2a2a');
+    //-------- ----------
+    // GRID
+    //-------- ----------
+    const grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    grid.material.linewidth = 3;
+    scene.add( grid );
+    //-------- ----------
+    // A MAIN SEQ OBJECT
+    //-------- ----------
+    const v3Array_campos = QBV3Array([
+        [8,8,8, 7,-2,-7,    2,0,0,      20],
+        [7,-2,-7, -8,4,0,   0,0,0,      25],
+        [-8,4,0, 8,8,8,     0,0,0,      50]
+    ]);
+    // PATH DEBUG POINTS
+    const points_debug = new THREE.Points(
+        new THREE.BufferGeometry().setFromPoints(v3Array_campos),
+        new THREE.PointsMaterial({size: 0.25, color: new THREE.Color(0,1,0)})
+    );
+    scene.add(points_debug);
+    // start options for main seq object
+    const opt_seq = {
+        fps: 30,
+        beforeObjects: function(seq){
+            camera.position.set(8, 8, 8);
+            camera.zoom = 1;
+        },
+        afterObjects: function(seq){
+            camera.updateProjectionMatrix();
+        },
+        objects: []
+    };
+    // SEQ 0 - ...
+    opt_seq.objects[0] = {
+        secs: 3,
+        update: function(seq, partPer, partBias){
+            // camera
+            //camera.position.set(5, 18, 14);
+            camera.lookAt(0, 0, 0);
+        }
+     };
+    // SEQ 1 - ...
+    opt_seq.objects[1] = {
+        secs: 7,
+        v3Paths: [
+            { key: 'campos', array: v3Array_campos, lerp: true }
+        ],
+        update: function(seq, partPer, partBias){
+            // camera
+            seq.copyPos('campos', camera);
+            camera.lookAt(0, 0, 0);
+        }
+     };
+    // SEQ 2 - ...
+    opt_seq.objects[2] = {
+        secs: 20,
+        update: function(seq, partPer, partBias){
+            // camera
+            camera.position.set(8 - 16 * partBias, 8, 8);
+            camera.lookAt(0, 0, 0);
+            const b2 = seq.getSinBias(1, true);
+            camera.zoom = 1 + 7 * b2;
+        }
+    };
+    const seq = scene.userData.seq = seqHooks.create(opt_seq);
+    console.log('frameMax for main seq: ' + seq.frameMax);
+    sm.frameMax = seq.frameMax;
+};
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    const seq = scene.userData.seq;
+    seqHooks.setFrame(seq, sm.frame, sm.frameMax);
+};
+ 
