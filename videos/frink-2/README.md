20e139e0a4ed7452a4719708bea7c2752ef99a4b
dustinpfister <dustin.pfister@gmail.com>
2022-10-28 10:49:59 -0400

    frink2 video folder started

---
+++ videos/frink-2/README.md
@@ -0,0 +1,4 @@
+# frink-1
+
+The idea here is to create a video in which I am sinking up the mutation of a sphere with a corresponding audio track. For now I am not going to be anything fancy when it comes to getting sample data to help with this as that would seem to be a bit of a rabbit hole to get that working thus far.
+
--- videos/frink-2/videos/video1.js
+++ videos/frink-2/videos/video1.js
@@ -1,15 +1,52 @@
-// 'frink1' - Got the basic idea of what I wanted to do working.
+// video1 for 'frink2' - Continuing from 'frink1' this time adding object-grid-wrap, and working out camera angles for each seq
 // 
 // scripts
 VIDEO.scripts = [
    '../../../js/sequences-hooks/r2/sequences-hooks.js',
-   '../../../js/sphere-mutate/r2/sphere-mutate.js'
+   //'../../../js/canvas/r1/canvas.js',
+   '../../../js/sphere-mutate/r2/sphere-mutate.js',
+   '../../../js/object-grid-wrap/r2/object-grid-wrap.js',
+   '../../../js/object-grid-wrap/r2/effects/opacity2.js'
 ];
 // init
 VIDEO.init = function(sm, scene, camera){
     //-------- ----------
     // HELPERS - helper functions from sequence hook demos
+    //   ( see https://dustinpfister.github.io/2022/05/12/threejs-examples-sequence-hooks/ )
     //-------- ----------
+    // just a short hand for THREE.QuadraticBezierCurve3
+    const QBC3 = function(x1, y1, z1, x2, y2, z2, x3, y3, z3){
+        let vs = x1;
+        let ve = y1;
+        let vc = z1;
+        if(arguments.length === 9){
+            vs = new THREE.Vector3(x1, y1, z1);
+            ve = new THREE.Vector3(x2, y2, z2);
+            vc = new THREE.Vector3(x3, y3, z3);
+        }
+        return new THREE.QuadraticBezierCurve3( vs, vc, ve );
+    };
+    // QBDelta helper using QBC3
+    // this works by giving deltas from the point that is half way between
+    // the two start and end points rather than a direct control point for x3, y3, and x3
+    const QBDelta = function(x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+        const vs = new THREE.Vector3(x1, y1, z1);
+        const ve = new THREE.Vector3(x2, y2, z2);
+        // deltas
+        const vDelta = new THREE.Vector3(x3, y3, z3);
+        const vc = vs.clone().lerp(ve, 0.5).add(vDelta);
+        const curve = QBC3(vs, ve, vc);
+        return curve;
+    };
+    // QBV3Array
+    const QBV3Array = function(data) {
+        const v3Array = [];
+        data.forEach( ( a ) => {
+            const curve = QBDelta.apply(null, a.slice(0, a.length - 1))
+            v3Array.push( curve.getPoints( a[9]) );
+        })
+        return v3Array.flat();
+    };
     // frink adjust helper
     const frinkAdjust = function(mesh, uls, uld){
         const mud = mesh.userData;
@@ -43,12 +80,34 @@ VIDEO.init = function(sm, scene, camera){
     const mesh1 = sphereMutate.create({
         size: 2, w: 40, h: 40, material: material_sphere
     });
+    //mesh1.userData.muld = 2; // 'Max Unit Length Delta'
+    //mesh1.userData.uls = 0.25;
     scene.add(mesh1);
     sphereMutate.update(mesh1, 1, updateOpt1);
     //-------- ----------
     // BACKGROUND
     //-------- ----------
     scene.background = new THREE.Color('#2a2a2a');
+    //-------- ----------
+    // GRID
+    //-------- ----------
+    //const grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    //grid.material.linewidth = 3;
+    //scene.add( grid );
+    //-------- ----------
+    // A MAIN SEQ OBJECT
+    //-------- ----------
+    const v3Array_campos = QBV3Array([
+        [8,8,8, 7,-2,-7,    2,0,0,      20],
+        [7,-2,-7, -8,4,0,   0,0,0,      25],
+        [-8,4,0, 8,8,8,     0,0,0,      50]
+    ]);
+    // PATH DEBUG POINTS
+    //const points_debug = new THREE.Points(
+    //    new THREE.BufferGeometry().setFromPoints(v3Array_campos),
+    //    new THREE.PointsMaterial({size: 0.25, color: new THREE.Color(0,1,0)})
+    //);
+    //scene.add(points_debug);
     // start options for main seq object
     const opt_seq = {
         fps: 30,
@@ -114,6 +173,20 @@ VIDEO.init = function(sm, scene, camera){
             frinkAdjust(mesh1, a, 1 - a);
         }
     };
+
+/*
+    opt_seq.objects[1] = {
+        secs: 7,
+        v3Paths: [
+            { key: 'campos', array: v3Array_campos, lerp: true }
+        ],
+        update: function(seq, partPer, partBias){
+            // camera
+            seq.copyPos('campos', camera);
+            camera.lookAt(0, 0, 0);
+        }
+    };
+*/
     const seq = scene.userData.seq = seqHooks.create(opt_seq);
     console.log('frameMax for main seq: ' + seq.frameMax);
     sm.frameMax = seq.frameMax;
