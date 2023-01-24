11902e5c826565c136f265088ad2bfe9f7e494ac
dustinpfister <dustin.pfister@gmail.com>
2023-01-24 13:39:11 -0500

    dae paths

--- videos/timer-train/videos/video4-landscape.js
+++ videos/timer-train/videos/video4-landscape.js
@@ -26,10 +26,12 @@ VIDEO.init = function(sm, scene, camera){
     const URL_DAE_NUMS = '../../../dae/count_down_basic/cd4-nums.dae';
     const URL_DAE_SCENE = '../../../dae/trainset/land-1a.dae';
     const URL_DAE_TRAIN = '../../../dae/trainset/train-1.dae';
+    const URL_DAE_LANDSCAPE = '../../../dae/trainset/landscape-1.dae';
     // RESOURCE DAE PATHS
     const URL_DAE_NUMS_RESOURCE = '../../../dae/count_down_basic/';
     const URL_DAE_SCENE_RESOURCE = '../../../dae/trainset/';
     const URL_DAE_TRAIN_RESOURCE = '../../../dae/trainset/';
+    const URL_DAE_LANDSCAPE_RESOURCE = '../../../dae/trainset/';
     // TRAIN SETTINGS
     const TRAIN_Y_ADJUST = new THREE.Vector3(0,0.25,0);
     const TRAIN_LAPS = 4;
