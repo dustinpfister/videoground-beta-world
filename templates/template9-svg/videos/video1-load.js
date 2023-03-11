bf262f69f1a71f7e07f8cfda700261c69a2488f6
dustinpfister <dustin.pfister@gmail.com>
2023-03-11 15:07:25 -0500

    made some progress

--- templates/template9-svg/videos/video1-load.js
+++ templates/template9-svg/videos/video1-load.js
@@ -1,3 +1,81 @@
+// video1-load.js from template9-svg
+// scripts
+VIDEO.scripts = [
+   // CORE MODULES
+   '../../../js/sequences-hooks/r2/sequences-hooks.js',
+   '../../../js/canvas/r2/lz-string.js',
+   '../../../js/canvas/r2/canvas.js'
+];
+// init
+VIDEO.init = function(sm, scene, camera){
+    // ---------- ----------
+    // LIGHT
+    // ---------- ----------
+    const dl = new THREE.DirectionalLight(0xffffff, 0.8);
+    dl.position.set(-2, 1, 2);
+    scene.add(dl);
+    const al = new THREE.AmbientLight(0xffffff, 0.1);
+    scene.add(al);
+    //-------- ----------
+    // GRID
+    //-------- ----------
+const grid = new THREE.GridHelper(10, 10);
+scene.add(grid)
+
+
+    //-------- ----------
+    // BACKGROUND
+    //-------- ----------
+    const canObj = canvasMod.create({
+        size: 512,
+        draw: 'grid_palette',
+        palette: ['#000000', '#1f1f1f', '#00ffff'],
+        dataParse: 'lzstring64',
+        state: { w: 8, h: 5, data: 'AwGlEYyzNCVgpcmPit1mqvTsg===' }
+    });
+    // can use LZString to compress and decompress
+    //console.log( LZString.decompressFromBase64('AwGlEYyzNCVgpcmPit1mqvTsg===') );
+    // I want to repeat the texture
+    const texture = canObj.texture;
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(32, 24);
+    scene.background = texture;
+    //-------- ----------
+    // A MAIN SEQ OBJECT
+    //-------- ----------
+    // start options for main seq object
+    const opt_seq = {
+        fps: 30,
+        beforeObjects: function(seq){
+            // CAMERA DEFAULTS
+            camera.position.set(10, 10, 10);
+            camera.lookAt(0, 0, 0);
+            camera.zoom = 1.26;
+        },
+        afterObjects: function(seq){
+            camera.updateProjectionMatrix();
+        },
+        objects: []
+    };
+    // SEQ 0 - count down
+    opt_seq.objects[0] = {
+        secs: 30,
+        update: function(seq, partPer, partBias){
+        }
+    };
+    //-------- ----------
+    // SET FRAME MAX
+    //-------- ----------
+    const seq = scene.userData.seq = seqHooks.create(opt_seq);
+};
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    const seq = scene.userData.seq;
+    seqHooks.setFrame(seq, sm.frame, sm.frameMax);
+};
+
+
 // video1-load.js for template9-svg
 // scripts
 /*
