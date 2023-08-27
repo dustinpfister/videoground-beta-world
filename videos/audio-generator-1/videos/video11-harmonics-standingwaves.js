b010256ea35a0cd59e71220a06632cd3d343b96a
dustinpfister <dustin.pfister@gmail.com>
2023-08-27 11:49:57 -0400

    video11-harmonics started

---
+++ videos/audio-generator-1/videos/video11-harmonics-standingwaves.js
@@ -0,0 +1,56 @@
+/*    video11-harmonics-standingwaves - for audio-generator-1 project
+          * just looking into what the deal is with 'standing waves'
+ */
+//-------- ----------
+// SCRIPTS
+//-------- ----------
+VIDEO.scripts = [
+  '../js/samp_tools.js',
+  '../js/samp_create.js',
+  '../js/samp_draw.js'
+];
+//-------- ----------
+// INIT
+//-------- ----------
+VIDEO.init = function(sm, scene, camera){
+    sm.renderer.setClearColor(0x000000, 0.25);
+
+    const sound = scene.userData.sound = CS.create_sound({
+        waveform : (sampset, a_wave ) => {
+            const freq_int = Math.floor( sampset.frequency );
+            return Math.sin( Math.PI  * freq_int * a_wave )  * sampset.amplitude;
+        },
+        for_sampset: ( sampset, i, a_sound, opt ) => {
+            sampset.a_wave = a_sound * opt.secs % 1;
+            sampset.amplitude = 0.75;
+            sampset.frequency = 80;
+            return sampset;
+        },
+        secs: 1
+    });
+
+    sm.frameMax = sound.frames;
+
+};
+//-------- ----------
+// UPDATE
+//-------- ----------
+VIDEO.update = function(sm, scene, camera, per, bias){
+    return CS.write_frame_samples(scene.userData.sound, sm.frame, sm.filePath);
+};
+//-------- ----------
+// RENDER
+//-------- ----------
+VIDEO.render = function(sm, canvas, ctx, scene, camera, renderer){
+    const sound = scene.userData.sound;
+    const alpha = sm.frame / ( sm.frameMax - 1);
+    // background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+    // draw disp
+    DSD.draw( ctx, sound.array_disp, sound.opt_disp, sm.frame / ( sm.frameMax - 1 ) );
+    DSD.draw( ctx, sound.array_frame, sound.opt_frame, 0 );
+    // additional plain 2d overlay for status info
+    DSD.draw_info(ctx, sound, sm);
+};
+
