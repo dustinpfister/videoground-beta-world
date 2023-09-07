7673a48b389d7e90537fa912e47395d6b703350d
dustinpfister <dustin.pfister@gmail.com>
2023-09-07 14:30:56 -0400

    cool sound for video04-03-perframe-table

--- videos/audio-generator-1/videos/video04-03-perframe-table.js
+++ videos/audio-generator-1/videos/video04-03-perframe-table.js
@@ -1,5 +1,6 @@
 /*    video04-03-perframe-table.js - for audio-generator-1 project
           * using the table waveform, and frame by frame alphas
+          * worked out a sound that sounds like a car ( Rad Racer like )
  */
 //-------- ----------
 // SCRIPTS
@@ -14,6 +15,48 @@ VIDEO.scripts = [
 //-------- ----------
 VIDEO.init = function(sm, scene, camera){
     sm.renderer.setClearColor(0x000000, 0.25);
+
+
+    const speed_array = [];
+
+    let si = 0;
+    while(si < 50){
+        speed_array.push(si  / 100);
+        si += 1;
+    }
+
+    si = 50;
+    while(si > 20){
+        speed_array.push(si  / 100);
+        si -= 10;
+    }
+
+    si = 20;
+    while(si < 70){
+        speed_array.push(si  / 100);
+        si += 1;
+    }
+
+    si = 70;
+    while(si > 40){
+        speed_array.push(si  / 100);
+        si -= 10;
+    }
+
+    si = 40;
+    while(si < 100){
+        speed_array.push(si  / 100);
+        si += 1;
+    }
+
+    si = 0;
+    while(si < 50){
+        const n = Math.floor(si / 5);
+        speed_array.push(0.90 + THREE.MathUtils.seededRandom(n) * 0.10);
+        si += 1;
+    }
+
+
     const sound = scene.userData.sound = CS.create_sound({
         waveform : 'table',
         for_sampset: ( samp, i, a_sound, opt ) => {
@@ -22,13 +65,13 @@ VIDEO.init = function(sm, scene, camera){
             const frame = Math.floor(i / spf);
             const a_frame = (i % spf) / spf;
 
-            const a_speed = 0.5;
+            const a_speed = speed_array[ Math.floor( speed_array.length * a_sound ) ];
             return {
                 amplitude: 0.75,
                 a_wave : a_frame,
                 table: [
                     {  waveform: 'sin', frequency: 4, amplitude: 1.40 * a_speed },
-                    {  waveform: 'pulse', frequency: 4 + 4 * a_speed, amplitude: 1.00 * a_speed  }
+                    {  waveform: 'pulse', frequency: 4 + 4 * a_speed, amplitude: 0.75 * a_speed  }
                 ]
             };
         },
