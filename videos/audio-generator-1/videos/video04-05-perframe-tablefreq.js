d9738663ecca2d0e54557e9d80787270bf926291
dustinpfister <dustin.pfister@gmail.com>
2023-09-10 18:09:30 -0400

    made some progress with video04-06-perframe-tablefreq

--- videos/audio-generator-1/videos/video04-05-perframe-tablefreq.js
+++ videos/audio-generator-1/videos/video04-05-perframe-tablefreq.js
@@ -16,34 +16,44 @@ VIDEO.init = function(sm, scene, camera){
     sm.renderer.setClearColor(0x000000, 0.25);
 
 
+    const data_freq = [
+        2,4,2,4,2,4,2,4, 8,7,6,8,7,6,
+        2,4,2,4,2,4,2,4, 8,7,6,8,7,6
+    ];
+
     const sound = scene.userData.sound = CS.create_sound({
-        waveform : (sampeset, a_wave ) => {
-            const table_count = sampeset.table.length;
+        waveform : (samp, a_wave ) => {
+            const table_count = samp.table.length;
+            const freq = samp.frequency === undefined ? 1 : samp.frequency;
             let i_wf = 0;
-            let samp = 0;
+            let s = 0;
             while(i_wf < table_count ){
-                const wf = sampeset.table[i_wf];
-                const wf_samp = CS.WAVE_FORM_FUNCTIONS[wf.waveform](wf, a_wave);
-                samp += wf_samp;
+                const wf = samp.table[i_wf];
+                const wf_samp = CS.WAVE_FORM_FUNCTIONS[wf.waveform](wf, a_wave * freq % 1);
+                s += wf_samp;
                 i_wf += 1;
             }
-            return ( samp / table_count ) * sampeset.amplitude;
+            return ( s / table_count ) * samp.amplitude;
         },
         for_sampset: ( samp, i, a_sound, opt ) => {
 
             const spf = opt.sound.samples_per_frame;
+            
             const frame = Math.floor(i / spf);
+            const a_sound2 = frame / (opt.secs * 30);
             const a_frame = (i % spf) / spf;
             return {
                 amplitude: 0.75,
+                frequency: data_freq[ Math.floor( data_freq.length * a_sound2) ],
                 a_wave : a_frame,
                 table: [
-                    {  waveform: 'sin', frequency: 4, amplitude: 0.5 }
+                    {  waveform: 'sin', frequency: 2, amplitude: 1.5 },
+                    {  waveform: 'sin', frequency: 8, amplitude: 0.5 }
                 ]
             };
         },
         //sample_rate: 1000,
-        disp_step: 1,
+        disp_step: 10,
         secs: 10
     });
 
