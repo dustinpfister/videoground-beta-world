7c3134035b1f95ba8cc5173331d7fc2f07f66824
dustinpfister <dustin.pfister@gmail.com>
2023-09-13 16:52:25 -0400

    audio-generator-tune-seededrandom project folder started

---
+++ videos/audio-generator-tune-seededrandom/todo.md
@@ -0,0 +1,6 @@
+# audio-generator-tune-seededrandom
+
+<!-- ----------
+  DONE
+----------- -->
+* ( done 09/13/2023 ) - started this for post folder
---
+++ videos/audio-generator-tune-seededrandom/videos/video01-01-original-woundup.js
@@ -0,0 +1,175 @@
+/*    video01-01-original-woundup - for audio-generator-tune-seededrandom project
+          * First tune for the project
+ */
+//-------- ----------
+// SCRIPTS
+//-------- ----------
+VIDEO.scripts = [
+  '../js/samp_tools.js',
+  '../js/samp_create.js',
+  '../js/samp_draw.js'
+];
+//-------- ----------
+// INIT
+//-------- ----------
+VIDEO.init = function(sm, scene, camera){
+    sm.renderer.setClearColor(0x000000, 0.25);
+
+    const sq = {
+        objects: []
+    };
+
+    sq.objects[0] = {
+        alpha: 5 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 40;
+            samp.frequency = 1 + 4 * a_object;
+            return samp;  
+        }
+    };
+
+    sq.objects[1] = {
+        alpha: 7 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 40;
+            samp.frequency = 5;
+            return samp;
+        }
+    };
+
+    sq.objects[2] = {
+        alpha: 10 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 40 - 30 * a_object;
+            samp.frequency = 5;
+            return samp;
+        }
+    };
+
+    sq.objects[3] = {
+        alpha: 15 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 10 + 90 * a_object;
+            samp.frequency = 5;
+            return samp;
+        }
+    };
+
+    sq.objects[4] = {
+        alpha: 20 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 100 - 90 * Math.sin( Math.PI * ( a_object * 8 % 1 ) );
+            samp.frequency = 5 - 4.5 * a_object;
+            return samp;
+        }
+    };
+
+    sq.objects[5] = {
+        alpha: 27 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 100 - 60 * a_object;
+            samp.frequency = 0.5;
+            return samp;
+        }
+    };
+
+    sq.objects[6] = {
+        alpha: 30 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 40;
+            samp.frequency = 0.5 + 0.5 * a_object;
+            return samp;
+        }
+    };
+
+
+    const tune = [
+        2.0, 1.0, 0.5, 2.0, 1.0, 0.5, 2.0, 2.0, 0.5, 0.5, 0.5, 0.5,
+        2.0, 1.0, 0.5, 2.0, 1.0, 0.5, 2.0, 2.0, 0.5, 0.5, 0.5, 0.5,
+        2.0, 1.0, 0.5, 2.0, 1.0, 0.5, 2.0, 2.0, 0.5, 0.5, 0.5, 0.5,
+        2.0, 1.0, 0.5, 2.0, 1.0, 0.5, 2.0, 2.0, 0.5, 0.5, 0.5, 0.5,
+        2.0, 1.0, 0.5, 2.0, 1.0, 0.5, 2.0, 2.0, 0.5, 0.5, 0.5, 0.5,
+        2.0, 1.0, 0.5, 2.0, 1.0, 0.5, 2.0, 2.0, 0.5, 0.5, 0.5, 0.5
+    ];
+
+    sq.objects[7] = {
+        alpha: 40 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 40 - 20 * a_sound;
+            samp.frequency = tune[ Math.floor( tune.length  * a_object ) ];
+            return samp;
+        }
+    };
+
+    sq.objects[8] = {
+        alpha: 60 / 60,
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            samp.values_per_wave = 20  + 80 * a_sound;
+            samp.frequency = 0.5;
+            return samp;
+        }
+    };
+
+    const applySQ = ( sq, samp, i, a_sound, opt ) => {
+        let i2 = 0;
+        const len = sq.objects.length;
+        let a_base = 0;
+        while( i2 < len ){
+            const obj = sq.objects[i2];
+            if( a_sound <= obj.alpha ){
+                let a_object = ( a_sound - a_base ) /  ( obj.alpha - a_base );
+                return obj.for_sampset(samp, i, a_sound, opt, a_object, sq);
+            }
+            a_base = obj.alpha;
+            i2 += 1;
+        }
+    };
+
+
+    const sound = scene.userData.sound = CS.create_sound({
+        waveform : 'seedednoise',
+        for_sampset: ( samp, i, a_sound, opt ) => {
+
+            const spf = opt.sound.samples_per_frame;
+            const frame = Math.floor(i / spf);
+            const a_frame = (i % spf) / spf;
+            samp.a_wave = a_frame;
+
+            //const a_sound3 = frame / opt.sound.frames;
+
+            samp.values_per_wave = 40;
+            samp.frequency = 1;
+            samp.amplitude = 0.75;
+
+            applySQ(sq, samp, i, a_sound, opt);
+
+            return samp;
+        },
+        disp_step: 100,
+        getsamp_lossy: DSD.getsamp_lossy_random,
+        secs: 60
+    });
+    sm.frameMax = sound.frames;
+};
+//-------- ----------
+// UPDATE
+//-------- ----------
+VIDEO.update = function(sm, scene, camera, per, bias){
+    return CS.write_frame_samples(scene.userData.sound, sm.frame, sm.filePath, true);
+};
+//-------- ----------
+// RENDER
+//-------- ----------
+VIDEO.render = function(sm, canvas, ctx, scene, camera, renderer){
+    const sound = scene.userData.sound;
+    const alpha = sm.frame / ( sm.frameMax - 1);
+    // background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+    // draw disp
+    DSD.draw( ctx, sound.array_disp, sound.opt_disp, sm.frame / ( sm.frameMax - 1 ) );
+    DSD.draw( ctx, sound.array_frame, sound.opt_frame, 0 );
+    // additional plain 2d overlay for status info
+    DSD.draw_info(ctx, sound, sm);
+};
+
