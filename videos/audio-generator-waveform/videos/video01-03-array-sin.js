373a69db71577c6e6e4a160937ea2a30095fa6f5
dustinpfister <dustin.pfister@gmail.com>
2023-09-25 12:17:31 -0400

    I think that will be it for video01-03-array-sin

--- videos/audio-generator-waveform/videos/video01-03-array-sin.js
+++ videos/audio-generator-waveform/videos/video01-03-array-sin.js
@@ -20,32 +20,31 @@ VIDEO.init = function(sm, scene, camera){
         objects: []
     };
 
-    sq.objects[0] = {
-        alpha: 2 / 5,
-        for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
-            fs.i_obj = 0;
-            return fs;
-        },
-        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
-            samp.amplitude = 1;
-            samp.frequency = 2;
-            return samp;  
-        }
-    };
+    const tune = [2, 3, 4, 5, 6, 7, 8, 2, 2, 4, 4, 3, 2, 5, 5, 5, 5, 5]
 
-    sq.objects[1] = {
-        alpha: 5 / 5,
-        for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
-            fs.i_obj = 1;
-            return fs;
-        },
-        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
-            samp.amplitude = 1;
-            samp.frequency = 3;
-            return samp;  
-        }
+    const total_secs = 15;
+    let i_obj = 0;
+    const len = 3;
+    while(i_obj < len){
+        (function(i_obj){
+            const secs = total_secs / len * (i_obj + 1);
+            sq.objects[i_obj] = {
+                alpha: secs / total_secs,
+                for_frame : (fs, frame, max_frame, a_sound2, opt, a_object, sq ) => {
+                    fs.i_obj = i_obj;
+                    fs.count_h = 20 + 80 * i_obj ;
+                    fs.amps = [2.70, 1.50, 1.00, 1.70];
+                    return fs;
+                },
+                for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+                    samp.amplitude = 1.0;
+                    samp.frequency = tune[ Math.floor( tune.length * a_object ) ] //3 + Math.floor(3 * a_object);
+                    return samp;  
+                }
+        };
+        }(i_obj));
+        i_obj += 1;
     }
-
 
     const func = (a_h, a1=0.45, a2 = 0.55, amp=1, a_off=0) => {
         let s1 = 0;
@@ -56,32 +55,31 @@ VIDEO.init = function(sm, scene, camera){
         return s1;
     };
 
-
     const sound = scene.userData.sound = CS.create_sound({
         waveform : 'array',
         for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
 
-
             ST.applySQFrame(sq, fs, frame, max_frame, a_sound2, opt);
 
-            console.log('0: ' + frame);
-            console.log(fs);
-            console.log('');
+            fs.amps = fs.amps || [1.00, 1.00, 1.00, 1.00];
+            fs.count_h = fs.count_h || 200;
+            //console.log(fs.i_obj + ' : ' + frame);
+            //console.log(fs);
+            //console.log('');
 
-            const a_note = a_sound2 * 10 % 1;
+            const a_note = a_sound2 * (sq.objects.length * tune.length) % 1;
             const array_wave = [];
             let i_h = 0;
-            const count_h = 200;
-            while(i_h < count_h){
-                const a_h = i_h / count_h;
+            while(i_h < fs.count_h){
+                const a_h = i_h / fs.count_h;
                 const amp = Math.sin(Math.PI  * a_note );
                 const a1 = 0.45 - 0.25 * a_note;
                 const a2 = 0.55 + 0.25 * a_note;
-                const s1 = func(a_h, 0.45, 0.55, amp * 1.00, 0);
-                const s2 = func(a_h, a1 , a2, amp * 1.00, 0);
+                const s1 = func(a_h, 0.45, 0.55, amp * fs.amps[0], 0);
+                const s2 = func(a_h, a1 , a2, amp * fs.amps[1], 0);
                 const d1 = 0.25 * a_note;
-                const s3 = func(a_h, a1 + d1 * 2, a2 + d1 , amp * 1.00, 0);
-                const s4 = func(a_h, a1 - d1, a2 - d1 * 2 , amp * 1.00, 0);
+                const s3 = func(a_h, a1 + d1 * 2, a2 + d1 , amp * fs.amps[2], 0);
+                const s4 = func(a_h, a1 - d1, a2 - d1 * 2 , amp * fs.amps[3], 0);
                 array_wave.push( ( s1 + s2 + s3 + s4 ) / 4 );
                 i_h += 1;
             }
@@ -103,7 +101,7 @@ VIDEO.init = function(sm, scene, camera){
         },
         getsamp_lossy: DSD.getsamp_lossy_pingpong,
         disp_step: 10,
-        secs: 5
+        secs: total_secs
     });
 
     sm.frameMax = sound.frames;
