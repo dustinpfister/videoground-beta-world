1373bd2993dfa6e6ac945610c5658918cd724479
dustinpfister <dustin.pfister@gmail.com>
2023-10-02 15:41:57 -0400

    js folder and smaple writer file

---
+++ videos/audio-generator-waveplane/js/samp-writer.js
@@ -0,0 +1,72 @@
+
+//-------- ----------
+// SAMP DATA
+//-------- ----------
+
+const SW ={};
+
+SW.normal_to_int16 = ( samp = 0 ) => {
+    return -32768 + 65535 * THREE.MathUtils.clamp(samp, 0, 1);
+};
+
+// build wav file header... Base on what I found here: https://gist.github.com/also/900023
+SW.build_wave_header = (opts) => {
+    var numFrames = opts.numFrames || 0;     // default to 0 frames
+    var numChannels = opts.numChannels || 1; // default to 'mono' (dp edit)
+    var sampleRate = opts.sampleRate || 44100;
+    var bytesPerSample = opts.bytesPerSample || 2;
+    var blockAlign = numChannels * bytesPerSample;
+    var byteRate = sampleRate * blockAlign;
+    var dataSize = numFrames * blockAlign;
+    var buffer = new ArrayBuffer(44);
+    var dv = new DataView(buffer);
+    var p = 0;
+    function writeString(s) {
+        for (var i = 0; i < s.length; i++) {
+            dv.setUint8(p + i, s.charCodeAt(i));
+        }
+        p += s.length;
+    }
+    function writeUint32(d) {
+        dv.setUint32(p, d, true);
+        p += 4;
+    }
+    function writeUint16(d) {
+        dv.setUint16(p, d, true);
+        p += 2;
+    }
+    writeString('RIFF');              // ChunkID
+    writeUint32(dataSize + 36);       // ChunkSize
+    writeString('WAVE');              // Format
+    writeString('fmt ');              // Subchunk1ID
+    writeUint32(16);                  // Subchunk1Size
+    writeUint16(1);                   // AudioFormat
+    writeUint16(numChannels);         // NumChannels
+    writeUint32(sampleRate);          // SampleRate
+    writeUint32(byteRate);            // ByteRate
+    writeUint16(blockAlign);          // BlockAlign
+    writeUint16(bytesPerSample * 8);  // BitsPerSample
+    writeString('data');              // Subchunk2ID
+    writeUint32(dataSize);            // Subchunk2Size
+    return buffer;
+};
+
+SW.write_frame_samples = ( data_samples = [], frame=0, filePath, total_secs=5, sample_rate=8000 ) => {
+    const clear = frame === 0 ? true: false;
+    const fn = 'video.wav';
+    const uri = videoAPI.pathJoin(filePath, fn);
+    if( frame === 0 ){
+        const numChannels = 1;
+        const array_header = SW.build_wave_header({
+            numFrames: sample_rate * numChannels * total_secs,
+            numChannels: numChannels,
+            sampleRate: sample_rate,
+            bytesPerSample: 2
+        });
+        return videoAPI.write(uri, new Int16Array( array_header ), true )
+        .then(()=>{
+            return videoAPI.write(uri, new Int16Array( data_samples ), false );
+        });
+    }
+    return videoAPI.write(uri, new Int16Array(data_samples), clear );
+};
--- videos/audio-generator-waveplane/videos/video01-01-alpha-hello.js
+++ videos/audio-generator-waveplane/videos/video01-01-alpha-hello.js
@@ -1,7 +1,12 @@
 /*    video01-01-alpha-hello - for audio-generator-waveplane project
           * Just Getting the core idea of what I want working
  */
-
+//-------- ----------
+// SCRIPTS
+//-------- ----------
+VIDEO.scripts = [
+  '../js/samp-writer.js'
+];
 //-------- ----------
 // WP OBJECT
 //-------- ----------
@@ -67,7 +72,7 @@ const gen_sampdata_tracks = (wp, sample_count=100, int16=true, mix_amp=1) => {
             const i_pos = i_tp * wp.samp_points + i_sp;
             let n = parseFloat( pos.getY(i_pos).toFixed(2)  );
             if(int16){
-                n = normal_to_int16( (n + 1) / 2 );
+                n = SW.normal_to_int16( (n + 1) / 2 );
             }
             samp_tracks[i_tp] = samp_tracks[i_tp] === undefined ? [] : samp_tracks[i_tp];
             samp_tracks[i_tp][i_samp] = n;
@@ -91,76 +96,6 @@ const gen_sampdata_tracks = (wp, sample_count=100, int16=true, mix_amp=1) => {
 };
 
 //-------- ----------
-// SAMP DATA
-//-------- ----------
-
-    const normal_to_int16 = ( samp = 0 ) => {
-        return -32768 + 65535 * THREE.MathUtils.clamp(samp, 0, 1);
-    };
-
-    // build wav file header... Base on what I found here: https://gist.github.com/also/900023
-    const build_wave_header = (opts) => {
-        var numFrames = opts.numFrames || 0;     // default to 0 frames
-        var numChannels = opts.numChannels || 1; // default to 'mono' (dp edit)
-        var sampleRate = opts.sampleRate || 44100;
-        var bytesPerSample = opts.bytesPerSample || 2;
-        var blockAlign = numChannels * bytesPerSample;
-        var byteRate = sampleRate * blockAlign;
-        var dataSize = numFrames * blockAlign;
-        var buffer = new ArrayBuffer(44);
-        var dv = new DataView(buffer);
-        var p = 0;
-        function writeString(s) {
-            for (var i = 0; i < s.length; i++) {
-                dv.setUint8(p + i, s.charCodeAt(i));
-            }
-            p += s.length;
-        }
-        function writeUint32(d) {
-            dv.setUint32(p, d, true);
-            p += 4;
-        }
-        function writeUint16(d) {
-            dv.setUint16(p, d, true);
-            p += 2;
-        }
-        writeString('RIFF');              // ChunkID
-        writeUint32(dataSize + 36);       // ChunkSize
-        writeString('WAVE');              // Format
-        writeString('fmt ');              // Subchunk1ID
-        writeUint32(16);                  // Subchunk1Size
-        writeUint16(1);                   // AudioFormat
-        writeUint16(numChannels);         // NumChannels
-        writeUint32(sampleRate);          // SampleRate
-        writeUint32(byteRate);            // ByteRate
-        writeUint16(blockAlign);          // BlockAlign
-        writeUint16(bytesPerSample * 8);  // BitsPerSample
-        writeString('data');              // Subchunk2ID
-        writeUint32(dataSize);            // Subchunk2Size
-        return buffer;
-    };
-
-    const write_frame_samples = ( data_samples = [], frame=0, filePath, total_secs=5, sample_rate=8000 ) => {
-        const clear = frame === 0 ? true: false;
-        const fn = 'video.wav';
-        const uri = videoAPI.pathJoin(filePath, fn);
-        if( frame === 0 ){
-            const numChannels = 1;
-            const array_header = build_wave_header({
-                numFrames: sample_rate * numChannels * total_secs,
-                numChannels: numChannels,
-                sampleRate: sample_rate,
-                bytesPerSample: 2
-            });
-            return videoAPI.write(uri, new Int16Array( array_header ), true )
-            .then(()=>{
-                return videoAPI.write(uri, new Int16Array( data_samples ), false );
-            });
-        }
-        return videoAPI.write(uri, new Int16Array(data_samples), clear );
-    };
-
-//-------- ----------
 // INIT
 //-------- ----------
 VIDEO.init = function(sm, scene, camera){
@@ -171,9 +106,8 @@ VIDEO.init = function(sm, scene, camera){
     scene.add(wp.mesh);
 
 
-    const sampdata_tracks = gen_sampdata_tracks(wp, 100);
-
-console.log(sampdata_tracks);
+    //const sampdata_tracks = gen_sampdata_tracks(wp, 100);
+    //console.log(sampdata_tracks);
 
     // start state for camera
     camera.position.set( 10, 8, 10);
@@ -202,7 +136,7 @@ VIDEO.update = function(sm, scene, camera, per, bias){
     const mix_amp = 1.0;
 
     const sampdata_tracks = gen_sampdata_tracks(wp, sud.samples_per_frame, true, mix_amp);
-    return write_frame_samples(sampdata_tracks.mixed, sm.frame, sm.filePath, sud.total_secs, sud.sample_rate );
+    return SW.write_frame_samples(sampdata_tracks.mixed, sm.frame, sm.filePath, sud.total_secs, sud.sample_rate );
 };
 //-------- ----------
 // RENDER
