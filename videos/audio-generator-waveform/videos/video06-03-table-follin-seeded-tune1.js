958cb628fe3926d114ca47b98fd4813324282af5
dustinpfister <dustin.pfister@gmail.com>
2023-10-16 13:04:22 -0400

    video06-03-table-follin-seeded-tune1 sounds okay

--- videos/audio-generator-waveform/videos/video06-03-table-follin-seeded-tune1.js
+++ videos/audio-generator-waveform/videos/video06-03-table-follin-seeded-tune1.js
@@ -18,35 +18,93 @@ VIDEO.scripts = [
 VIDEO.init = function(sm, scene, camera){
     sm.renderer.setClearColor(0x000000, 0.25);
 
+    const sq = {
+        objects: []
+    };
+
+    const total_secs = 25;
+    const playback_secs = total_secs; //5;
+
+    sq.objects[0] = {
+        alpha: 5 / total_secs,
+        for_frame: (fs, frame, max_frame, a_sound2, opt, a_object, sq) => {
+            // 'seedednoise' options
+            fs.amp_sn = 1 - 0.75 * a_object;
+            fs.freq_sn = 1;
+            fs.values_per_wave = Math.floor(200 - 160 * a_object);
+            fs.int_shift = 0;
+            // 'follinsaw' options
+            const a_duty = ST.get_alpha_sin(a_object, 1, (10 - 8 * a_object ) * opt.secs);
+            fs.freq_fs = 10 - 6 * a_object;
+            fs.amp_fs = 1;
+            fs.p1 = 0.75 - 0.25 * a_object;
+            fs.p2 = 0.25 + 0.25 * a_object;
+            fs.duty = 0.75 - 0.25 * a_duty;
+            return fs;
+        },
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            return samp;  
+        }
+    };
+    const tune_fs = [
+        4,4,4,4, 4,4,4,4, 4,4,4,4, 5,5,5,5, 5,5,5,5,
+        5,5,5,5, 0,4,4,0, 0,4,4,0, 0,5,5,5, 5,5,5,5,
+        5,5,5,0, 0,4,4,0, 0,4,4,0, 0,5,5,0, 0,5,5,0,
+        0,4,4,0, 0,4,4,0, 0,5,5,0, 0,5,5,4, 4,2,2,1
+    ];
+    const tune_seed = [
+        1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 2,2,2,2,
+        2,2,2,2, 2,2,2,2, 2,2,2,2, 1,1,1,1, 1,1,1,1,
+        3,3,3,3, 3,3,3,3, 2,2,2,2, 2,2,2,2, 1,1,1,1,
+        1,1,1,1, 1,1,2,2, 3,3,3,3, 2,2,1,1, 1,1,1,1
+    ];
+    sq.objects[1] = {
+        alpha: 25 / total_secs,
+        for_frame: (fs, frame, max_frame, a_sound2, opt, a_object, sq) => {
+            // common alphas
+            const a1 = a_sound2 * (opt.secs * 1 ) % 1;
+            // 'seedednoise' options
+            const ti1 = 0.25 + 0.25 * tune_seed[ Math.floor(tune_seed.length * a_object) ];
+            fs.amp_sn = 0.25 + 0.75 * a_object;
+            fs.freq_sn = ti1;
+            fs.values_per_wave = 40;
+            fs.int_shift = 0;
+            // 'follinsaw' options
+            const a_duty = ST.get_alpha_sin(a_object, 1, 2 * opt.secs);
+            const ti2 = tune_fs[ Math.floor(tune_fs.length * a_object) ];
+            fs.freq_fs = ti2;
+            fs.amp_fs = ti2 > 0 ? 1: 0;
+            fs.p1 = 0.75 - 0.25 * a_object;
+            fs.p2 = 0.25 + 0.25 * a_object;
+            fs.duty = 0.75 - 0.25 * a_duty;
+            return fs;
+        },
+        for_sampset: function(samp, i, a_sound, opt, a_object, sq){
+            return samp;  
+        }
+    };
+
     const sound = scene.userData.sound = CS.create_sound({
         waveform : 'array',
         // called once per frame
         for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
 
+            // default waveform options
             fs.array_wave = scene.userData.array_wave = [];
             fs.freq = 1;
             fs.amp = 1;
+            fs.amp_sn = 0.1;
+            fs.freq_sn = 1;
+            fs.values_per_wave = 40;
+            fs.int_shift = 0;
+            fs.freq_fs = 2;
+            fs.amp_fs = 1;
+            fs.p1 = 0.75;
+            fs.p2 = 0.25;
+            fs.duty = 0.5;
 
-            // common alphas
-            const a1 = a_sound2 * (opt.secs * 1 ) % 1;
-
-            // 'seedednoise' options
-            const a_sin = Math.sin( Math.PI * a1 );
-            const amp_sn = 0;
-            const freq_sn = 0;
-            const values_per_wave = 40;
-            const int_shift = 0;
-
-            // 'follinsaw' options
-            const a_duty = a_sound2 * opt.secs % 1;
-
-
-            const freq_fs = 2;
-            const amp_fs = 1;
-            const p1 = 0.95
-            const p2 = 0.25;
-            const duty = 0.9 - 0.6 * a_duty;
-
+            // apply logic for current frame
+            ST.applySQFrame(sq, fs, frame, max_frame, a_sound2, opt);
 
             // table samp object
             const samp_table = {
@@ -55,10 +113,19 @@ VIDEO.init = function(sm, scene, camera){
                 maxch: 2,
                 a_wave : a_sound2,
                 table: [
-                    { waveform: 'seedednoise', frequency: freq_sn, amplitude: amp_sn,
-                      values_per_wave: values_per_wave, int_shift: int_shift },
-                    { waveform: 'follinsaw',   frequency: freq_fs, amplitude: amp_fs,
-                      duty: duty, p1: p1, p2: p2 },
+                    { waveform: 'seedednoise',
+                      frequency: fs.freq_sn,
+                      amplitude: fs.amp_sn,
+                      values_per_wave: fs.values_per_wave,
+                      int_shift: fs.int_shift
+                    },
+                    { waveform: 'follinsaw',
+                      frequency: fs.freq_fs,
+                      amplitude: fs.amp_fs,
+                      duty: fs.duty,
+                      p1: fs.p1,
+                      p2: fs.p2
+                    }
                 ]
             };
 
@@ -86,10 +153,11 @@ VIDEO.init = function(sm, scene, camera){
             samp.a_wave = a_frame;
             samp.amplitude = fs.amp;
             samp.frequency = fs.freq;
+            ST.applySQ(sq, samp, i, a_sound, opt);
             return samp;
         },
         disp_step: 100,
-        secs: 3
+        secs: playback_secs
  });
     sm.frameMax = sound.frames;
 };
