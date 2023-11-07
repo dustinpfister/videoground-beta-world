79d2418369b6e475d1275bcaf0258043ee279fd3
dustinpfister <dustin.pfister@gmail.com>
2023-11-07 07:38:49 -0500

    readme files of samp create js file, and video1 of samp-pitch-amp-curves

--- videos/samp-pitch-amp-curves/videos/video01-01-test-scale.js
+++ videos/samp-pitch-amp-curves/videos/video01-01-test-scale.js
@@ -9,6 +9,7 @@
 VIDEO.scripts = [
   '../../../js/samp_create/r0/samp_tools.js',
   '../../../js/samp_create/r0/samp_create.js',
+  '../../../js/samp_create/r0/waveforms/seedednoise.js',
   '../../../js/samp_create/r0/samp_draw.js'
 ];
 //-------- ----------
@@ -17,37 +18,17 @@ VIDEO.scripts = [
 VIDEO.init = function(sm, scene, camera){
     const sud = scene.userData;
     sm.renderer.setClearColor(0x000000, 0.25);
-
-
+    // curve for setting pitch over time
     const v_start = new THREE.Vector2(0,0);
     const v_end = new THREE.Vector2(1,0);
     const v_control = new THREE.Vector2(0.10,0.99);
     const curve = new THREE.QuadraticBezierCurve(v_start, v_control, v_end);
-
-    const sq = {
-        objects: []
-    };
-    const playback_secs = 10;
-    sq.objects[0] = {
-        alpha: 1,
-        for_frame: (fs, frame, max_frame, a_sound2, opt, a_object, sq) => {
-
-            const v2 = curve.getPoint(a_object);
-
-
-            fs.amp = 0.75;
-            fs.freq = (2 * Math.floor(20 * v2.y));
-
-
-            return fs;
-        }
-    };
     const sound = sud.sound = CS.create_sound({
-        waveform : 'sin2',
+        waveform : 'seedednoise',
         for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
-            fs.freq = 1;
-            fs.amp = 1;
-            ST.applySQFrame(sq, fs, frame, max_frame, a_sound2, opt);
+            const v2 = curve.getPoint(a_sound2);
+            fs.amp = 0.75;
+            fs.freq = ( 2 * Math.floor( 20 * v2.y ) );
             return fs;
         },
         for_sampset: ( samp, i, a_sound, fs, opt ) => {
@@ -59,10 +40,9 @@ VIDEO.init = function(sm, scene, camera){
             samp.a_wave = a_frame;
             samp.amplitude = fs.amp;
             samp.frequency = fs.freq;
-            ST.applySQ(sq, samp, i, a_sound, opt);
             return samp;
         },
-        secs: playback_secs
+        secs: 10
     });
     sud.opt_frame = { w: 1200, h: 150, sy: 500, sx: 40, mode: sound.mode };
     sm.frameMax = sound.frames;
