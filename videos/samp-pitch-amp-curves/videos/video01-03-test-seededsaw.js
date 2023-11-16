b3b9e76152d1dbb441f0dbc409812fd5fd5cb4bc
dustinpfister <dustin.pfister@gmail.com>
2023-11-16 10:27:24 -0500

    more work on video01-03-test-seededsaw of samp-pitch-amp-curves

--- videos/samp-pitch-amp-curves/videos/video01-03-test-seededsaw.js
+++ videos/samp-pitch-amp-curves/videos/video01-03-test-seededsaw.js
@@ -17,10 +17,8 @@ VIDEO.scripts = [
 VIDEO.init = function(sm, scene, camera){
 
     CS.WAVE_FORM_FUNCTIONS.seededsaw = (samp, a_wave )=> {
-
         samp.saw_effect = samp.saw_effect === undefined ? 0.5 : samp.saw_effect;
         samp.values_per_wave = samp.values_per_wave === undefined ? 40 : samp.values_per_wave;
-
         samp.int_shift = samp.int_shift === undefined ? 0 : samp.int_shift;
         samp.freq_alpha = samp.freq_alpha === undefined ? 1 : samp.freq_alpha;
         const freq_raw = samp.frequency;
@@ -28,7 +26,6 @@ VIDEO.init = function(sm, scene, camera){
         const a = a_wave * freq % 1;
         const i = Math.floor( (a * samp.values_per_wave) * 0.99999999 );
         const i2 = i + 1 % samp.values_per_wave;
-
         const b1 = -1 + 2 * THREE.MathUtils.seededRandom( samp.int_shift + i );
         const b2 = -1 + 2 * THREE.MathUtils.seededRandom( samp.int_shift + i2 );
         const n = THREE.MathUtils.lerp(b1, b2, a * (samp.values_per_wave * samp.saw_effect) % 1);
@@ -40,15 +37,19 @@ VIDEO.init = function(sm, scene, camera){
     sm.renderer.setClearColor(0x000000, 0.25);
     // curve paths
     const curve_freq = sud.curve_freq = ST.get_curve_path([ 
-        [0.00, 0.50, 0.13, 0.50, 0.13, 0.13, 0.13]
+        [0.00, 0.50, 0.13, 0.50, 0.13, 0.13, 0.13],
+        [0.50, 0.55, 0.20, 0.65, 1.00, 0.13, 1.00],
     ]);
-    const curve_amp = sud.curve_amp = ST.get_curve_path([ 
-        [0.00, 0.50, 0.80, 0.50, 0.50, 0.60, 0.80]
+    const curve_param1 = sud.curve_param1 = ST.get_curve_path([ 
+        [0.00, 0.20, 1.00, 0.70, 0.20, 0.10, 1.00]
     ]);
-    const curve_param = sud.curve_param = ST.get_curve_path([ 
+    const curve_param2 = sud.curve_param2 = ST.get_curve_path([ 
         [0.00, 0.05, 0.00, 0.05, 0.00, 0.00, 0.00],
-        [0.10, 0.45, 0.50, 0.45, 0.50, 0.00, 0.00],
-        [0.50, 0.70, 0.80, 0.70, 0.80, 0.00, 0.00],
+        [0.10, 0.15, 0.25, 0.15, 0.25, 0.00, 0.25],
+        [0.20, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25],
+        [0.30, 0.35, 0.50, 0.35, 0.50, 0.25, 0.50],
+        [0.40, 0.45, 0.50, 0.45, 0.50, 0.50, 0.50],
+        [0.50, 0.65, 1.00, 0.75, 1.00, 0.50, 1.00],
     ]);
     // main sound object
     const sound = sud.sound = CS.create_sound({
@@ -56,11 +57,12 @@ VIDEO.init = function(sm, scene, camera){
         for_frame : (fs, frame, max_frame, a_sound2, opt ) => {
             let v2_ca = ST.get_curve_v2ca(curve_freq, a_sound2);
             fs.freq = 8 * v2_ca.y;
-            v2_ca = ST.get_curve_v2ca(curve_amp, a_sound2);
-            fs.amp = v2_ca.y;
-            v2_ca = ST.get_curve_v2ca(curve_param, a_sound2);
-            fs.saw_effect = v2_ca.y;
-            fs.values_per_wave = 40;
+            v2_ca = ST.get_curve_v2ca(curve_param1, a_sound2);
+
+            fs.values_per_wave = Math.round(180 * v2_ca.y);
+            v2_ca = ST.get_curve_v2ca(curve_param2, a_sound2);
+            fs.saw_effect = Math.floor( 16 * v2_ca.y) / 16;
+            fs.amp = 0.75;
             return fs;
         },
         for_sampset: ( samp, i, a_sound, fs, opt ) => {
@@ -105,8 +107,8 @@ VIDEO.render = function(sm, canvas, ctx, scene, camera, renderer){
     // curve
     let alpha = sm.per
     DSD.draw_curve( ctx, sud.curve_freq, alpha, 'freq', sud.opt_curve_freq );
-    DSD.draw_curve( ctx, sud.curve_amp, alpha, 'amp', sud.opt_curve_amp );
-    DSD.draw_curve( ctx, sud.curve_param, alpha, 'param ( saw effect ) ', sud.opt_curve_param );
+    DSD.draw_curve( ctx, sud.curve_param1, alpha, 'param1 ( values per wave )', sud.opt_curve_amp );
+    DSD.draw_curve( ctx, sud.curve_param2, alpha, 'param2 ( saw effect ) ', sud.opt_curve_param );
     // draw frame disp, and info
     DSD.draw( ctx, sound.array_frame, sud.opt_frame, 0, 'sample data ( current frame )' );
     DSD.draw_info(ctx, sound, sm);
