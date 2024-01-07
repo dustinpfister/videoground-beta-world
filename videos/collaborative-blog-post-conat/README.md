7393f3b5632df94443d0ae205f32902ac0d2e5ae
dustinpfister <dustin.pfister@gmail.com>
2024-01-07 12:04:41 -0500

    started a collaborative-blog-post-conat project folder

---
+++ videos/collaborative-blog-post-conat/README.md
@@ -0,0 +1,4 @@
+# collaborative-blog-post-conat
+
+This is a collaborative project between myself and a friend of mine. My friend wanted me to create a work that is a few of my blog post video projects concatanted into a single video project. So for the most part my friend is doing a curation process of sorts with work that I all ready did in the past with my blog post embed projects. Also I am just reworking old code that I all ready have in place. Never the less this is still a collaborative project of sorts that I would like to do more of in the future.
+
---
+++ videos/collaborative-blog-post-conat/videos/video01-01-draft-core.js
@@ -0,0 +1,52 @@
+// video01-01-draft-core from collaborative-blog-post-conat
+// scripts
+VIDEO.scripts = [
+   '../../../js/sequences-hooks/r2/sequences-hooks.js'
+];
+// init
+VIDEO.init = function(sm, scene, camera){ 
+    //-------- ----------
+    // BACKGROUND
+    //-------- ----------
+    scene.background = new THREE.Color('#2a2a2a');
+    //-------- ----------
+    // GRID
+    //-------- ----------
+    const grid = scene.userData.grid = new THREE.GridHelper(10, 10, '#ffffff', '#00afaf');
+    grid.material.linewidth = 3;
+    scene.add( grid );
+    //-------- ----------
+    // A MAIN SEQ OBJECT
+    //-------- ----------
+    // start options for main seq object
+    const opt_seq = {
+        fps: 30,
+        beforeObjects: function(seq){
+            camera.position.set(8, 8, 8);
+            camera.zoom = 1;
+        },
+        afterObjects: function(seq){
+            camera.updateProjectionMatrix();
+        },
+        objects: []
+    };
+    // SEQ 0 - ...
+    opt_seq.objects[0] = {
+        secs: 3,
+        update: function(seq, partPer, partBias){
+
+camera.position.x = 8 - 16 * partPer;
+camera.lookAt(0,0,0);
+
+        }
+    };
+    const seq = scene.userData.seq = seqHooks.create(opt_seq);
+    console.log('frameMax for main seq: ' + seq.frameMax);
+    sm.frameMax = seq.frameMax;
+};
+// update method for the video
+VIDEO.update = function(sm, scene, camera, per, bias){
+    const seq = scene.userData.seq;
+    seqHooks.setFrame(seq, sm.frame, sm.frameMax);
+};
+ 
